e712ef51f2c2699ab917d9601a014e9aabbafde5
Favour Okeibunor <favour@hollatags.com>
2021-05-20 12:25:14 +0100

    add netlify functions

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,13 +1,53 @@
 
-const fetch = require('node-fetch')
+import fetch from "node-fetch"
 
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+const github_data = {
+    "token": process.env.API_KEY,
+    "username": "Okeibunor"
+}
+
+// GraphQL Query 
+const body = {
+    "query": `
+    query{
+      user(login: "Okeibunor") {
+        avatarUrl,
+        login,
+        name,
+        bio,
+        repositories(first: 20, orderBy: {field: UPDATED_AT, direction: DESC}) {
+          nodes {
+            name,
+            url,
+            description,
+            updatedAt,
+            forkCount,
+            stargazerCount,
+            primaryLanguage {
+              name,
+              color
+            }
+          }
+        }
+      }
+    }`
+}
+
+const baseUrl = "https://api.github.com/graphql";
+
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: "bearer " + github_data.token
+}
 
 exports.handler = async (event, context) => {
     let response
     try {
-        response = await fetch(API_ENDPOINT)
-        // handle response
+        response = await fetch(baseUrl, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(body)
+        }).then(response => response.json()).then(data => data)
     } catch (err) {
         return {
             statusCode: err.statusCode || 500,
@@ -16,7 +56,6 @@ exports.handler = async (event, context) => {
             })
         }
     }
-
     return {
         statusCode: 200,
         body: JSON.stringify({
@@ -24,3 +63,4 @@ exports.handler = async (event, context) => {
         })
     }
 }
+
---
+++ netlify/functions/test.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch"
+
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+
+exports.handler = async (event, context) => {
+    let response
+    try {
+        response = await fetch(API_ENDPOINT).then(res => res.json())
+        // handle resp
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data: response
+        })
+    }
+}
