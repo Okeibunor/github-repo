2c0244ef2cdd889d137b1f580127e0bef53ba21e
Favour Okeibunor <favour@hollatags.com>
2021-05-20 17:13:41 +0100

    correct base url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://github-repo-okeibunor.netlify.app";
+const baseUrl = "https://github-repo-okeibunor.netlify.app/.netlify/functions/getData";
 
 const headers = {
   "Content-Type": "application/json",
